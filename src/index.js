2317a02884315c15d31ae669539cc3f0363e65cc
0skat <81402903+0skat@users.noreply.github.com>
2021-10-08 18:08:07 -0700

    succesuflly generates graphs from user selection added styling to components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const { select } = require("d3");
 const songs = require ("./SongData.json");
 let chartVariable;
+let count = 0; 
 
 document.addEventListener("DOMContentLoaded", ()=>{
   options(); 
@@ -10,237 +11,237 @@ document.addEventListener("DOMContentLoaded", ()=>{
   // addElement(); 
 })
 
-function createAlbumInfo(){
-  let parent = document.querySelector(".albums"); 
-  let unordered = document.createElement("ul"); 
-  parent.appendChild(unordered); 
+// function createAlbumInfo(){
+//   let parent = document.querySelector(".albums"); 
+//   let unordered = document.createElement("ul"); 
+//   parent.appendChild(unordered); 
 
-  for(let i = 0; i < songs.length; i++){
+//   for(let i = 0; i < songs.length; i++){
     
-    let listElement = document.createElement("li"); 
-    listElement.dataset.index = i; 
-    unordered.appendChild(listElement); 
+//     let listElement = document.createElement("li"); 
+//     listElement.dataset.index = i; 
+//     unordered.appendChild(listElement); 
 
-    let image = document.createElement("img"); 
-    listElement.appendChild(image); 
+//     let image = document.createElement("img"); 
+//     listElement.appendChild(image); 
 
-    let imageSrc = songs[i].src; 
-    image.setAttribute('class', 'album-art')
-    image.setAttribute("src", imageSrc); 
+//     let imageSrc = songs[i].src; 
+//     image.setAttribute('class', 'album-art')
+//     image.setAttribute("src", imageSrc); 
 
-    listElement.addEventListener("click", (e)=> {
-      e.preventDefault(); 
-      let ulToDelete = document.querySelector(".infoDelete")
+//     listElement.addEventListener("click", (e)=> {
+//       e.preventDefault(); 
+//       let ulToDelete = document.querySelector(".infoDelete")
       
-      if(ulToDelete){
-        ulToDelete.remove();
-      } 
-      displayInfo(e); 
-    })
-  }
+//       if(ulToDelete){
+//         ulToDelete.remove();
+//       } 
+//       displayInfo(e); 
+//     })
+//   }
     
-}
+// }
 
-function displayInfo(e) {
-  displayInfo.called = true; 
+// function displayInfo(e) {
+//   displayInfo.called = true; 
 
-  let index = e.currentTarget.dataset.index; 
-  let placeData = document.querySelector(".popup");
-  let newList = document.createElement("ul")
-  newList.setAttribute("class", "infoDelete")
-  placeData.appendChild(newList)
-  let data = songs[index];
+//   let index = e.currentTarget.dataset.index; 
+//   let placeData = document.querySelector(".popup");
+//   let newList = document.createElement("ul")
+//   newList.setAttribute("class", "infoDelete")
+//   placeData.appendChild(newList)
+//   let data = songs[index];
   
 
-  let keys = Object.keys(data)
+//   let keys = Object.keys(data)
 
 
-  for(let i = 0; i < keys.length; i++){
-    let temp = keys[i];
-    let newOption = new Option(`${temp}: ${data[temp]}`, data[temp])
-    // placeData.innerHTML = `${temp}: ${data[temp]}`
-    newList.appendChild(newOption);
-  }
+//   for(let i = 0; i < keys.length; i++){
+//     let temp = keys[i];
+//     let newOption = new Option(`${temp}: ${data[temp]}`, data[temp])
+//     // placeData.innerHTML = `${temp}: ${data[temp]}`
+//     newList.appendChild(newOption);
+//   }
   
-}
+// }
 
 
 
 
   
-//methods to populate chart data 
-function createAccRows(arr) {
-  const accRows = []; 
-  for(let i = 0; i < arr.length; i++){
-    accRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].acousticness])
-  }
-  return accRows; 
-};
+// //methods to populate chart data 
+// function createAccRows(arr) {
+//   const accRows = []; 
+//   for(let i = 0; i < arr.length; i++){
+//     accRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].acousticness])
+//   }
+//   return accRows; 
+// };
 
-//dance-data 
-function createDanceRows(arr) {
-  const danceRows = []; 
-  for(let i = 0; i < arr.length; i++){
-    danceRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].danceability])
-  }
-  return danceRows; 
-};
+// //dance-data 
+// function createDanceRows(arr) {
+//   const danceRows = []; 
+//   for(let i = 0; i < arr.length; i++){
+//     danceRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].danceability])
+//   }
+//   return danceRows; 
+// };
 
-//Popular-data 
-function createPopularRows(arr) {
-  const popularityRows = []; 
-  for(let i = 0; i < arr.length; i++){
-    popularityRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].popularity])
-  }
-  return popularityRows; 
-};
+// //Popular-data 
+// function createPopularRows(arr) {
+//   const popularityRows = []; 
+//   for(let i = 0; i < arr.length; i++){
+//     popularityRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].popularity])
+//   }
+//   return popularityRows; 
+// };
 
-//Energy-data 
-function createEnergyRows(arr) {
-  const energyRows = []; 
-  for(let i = 0; i < arr.length; i++){
-    energyRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].energy])
-  }
-  return energyRows; 
-};
+// //Energy-data 
+// function createEnergyRows(arr) {
+//   const energyRows = []; 
+//   for(let i = 0; i < arr.length; i++){
+//     energyRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].energy])
+//   }
+//   return energyRows; 
+// };
 
-//Energy-data 
-function createTempoRows(arr) {
-  const tempoRows = []; 
-  for(let i = 0; i < arr.length; i++){
-    tempoRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].tempo])
-  }
-  return tempoRows; 
-};
+// //Energy-data 
+// function createTempoRows(arr) {
+//   const tempoRows = []; 
+//   for(let i = 0; i < arr.length; i++){
+//     tempoRows.push([arr[i].name + ", "+ arr[i].artists, arr[i].tempo])
+//   }
+//   return tempoRows; 
+// };
 
 
-//Accoustic Chart
-anychart.onDocumentReady(function() {
-  anychart.theme(anychart.themes.darkBlue)
-  // set the data
-  var data = {
-      header: ["Song Title", "Accouticness (dB)"],
-      rows: 
-        createAccRows(songs)
-  };
+// //Accoustic Chart
+// anychart.onDocumentReady(function() {
+//   anychart.theme(anychart.themes.darkBlue)
+//   // set the data
+//   var data = {
+//       header: ["Song Title", "Accouticness (dB)"],
+//       rows: 
+//         createAccRows(songs)
+//   };
 
-  // create the chart
-  var chart = anychart.column();
+//   // create the chart
+//   var chart = anychart.column();
 
-  // add the data
-  chart.data(data);
+//   // add the data
+//   chart.data(data);
 
-  // set the chart title
-  chart.title("Accoustic Levels, by Song");
+//   // set the chart title
+//   chart.title("Accoustic Levels, by Song");
 
-  // draw
-  chart.container("Accoustic");
-  chart.draw();
-});
+//   // draw
+//   chart.container("Accoustic");
+//   chart.draw();
+// });
 
-//Danceability chart 
-anychart.onDocumentReady(function() {
-  anychart.theme(anychart.themes.darkBlue)
+// //Danceability chart 
+// anychart.onDocumentReady(function() {
+//   anychart.theme(anychart.themes.darkBlue)
 
-  var data = {
-      header: ["Song Title", "Danceability"],
-      rows: 
-        createDanceRows(songs)
-  };
+//   var data = {
+//       header: ["Song Title", "Danceability"],
+//       rows: 
+//         createDanceRows(songs)
+//   };
 
-  var chart = anychart.column();
+//   var chart = anychart.column();
 
-  chart.data(data);
+//   chart.data(data);
 
-  chart.title("Danceability, by Song");
+//   chart.title("Danceability, by Song");
 
-  chart.container("Dance");
-  chart.draw();
-});
+//   chart.container("Dance");
+//   chart.draw();
+// });
 
-//Popularity chart 
-anychart.onDocumentReady(function() {
-  anychart.theme(anychart.themes.darkBlue)
+// //Popularity chart 
+// anychart.onDocumentReady(function() {
+//   anychart.theme(anychart.themes.darkBlue)
 
-  var data = {
-      header: ["Song Title", "Popularity"],
-      rows: 
-        createPopularRows(songs)
-  };
+//   var data = {
+//       header: ["Song Title", "Popularity"],
+//       rows: 
+//         createPopularRows(songs)
+//   };
 
-  var chart = anychart.column();
+//   var chart = anychart.column();
 
-  chart.data(data);
+//   chart.data(data);
 
-  chart.title("Popularity, by Song");
+//   chart.title("Popularity, by Song");
 
-  chart.container("Popular");
-  chart.draw();
-});
+//   chart.container("Popular");
+//   chart.draw();
+// });
 
-//Energy chart 
-anychart.onDocumentReady(function() {
+// //Energy chart 
+// anychart.onDocumentReady(function() {
 
-  anychart.theme(anychart.themes.darkBlue)
+//   anychart.theme(anychart.themes.darkBlue)
   
-  var data = {
-      header: ["Song Title", "Energy"],
-      rows: 
-      createEnergyRows(songs)
-  };
+//   var data = {
+//       header: ["Song Title", "Energy"],
+//       rows: 
+//       createEnergyRows(songs)
+//   };
 
-  var chart = anychart.column();
+//   var chart = anychart.column();
 
-  chart.data(data);
+//   chart.data(data);
 
-  chart.title("Energy, by Song");
+//   chart.title("Energy, by Song");
 
-  chart.container("Energy");
-  chart.draw();
-});
+//   chart.container("Energy");
+//   chart.draw();
+// });
 
-//Tempo chart 
-anychart.onDocumentReady(function() {
+// //Tempo chart 
+// anychart.onDocumentReady(function() {
 
-  anychart.theme(anychart.themes.darkBlue)
+//   anychart.theme(anychart.themes.darkBlue)
 
-  var data = {
-      header: ["Song Title", "Tempo"],
-      rows: 
-      createTempoRows(songs)
-  };
+//   var data = {
+//       header: ["Song Title", "Tempo"],
+//       rows: 
+//       createTempoRows(songs)
+//   };
 
-  var chart = anychart.column();
-
-
-  chart.data(data);
-
-  chart.title("Tempo, by Song");
-
-  chart.container("Tempo");
-  chart.draw();
-});
+//   var chart = anychart.column();
 
 
-function getName(){
+//   chart.data(data);
 
-  let userInput = document.querySelector(".artistName").value
-  for(let i = 0; i < songs.length; i++){
-    if(songs[i].artists === `${userInput}`)
-    displaySearch(songs[i]);  
-  }
-  return "Sorry That Artist Info is not available"; 
-}; 
+//   chart.title("Tempo, by Song");
+
+//   chart.container("Tempo");
+//   chart.draw();
+// });
 
 
+// function getName(){
 
-function search(){
-  let button = document.querySelector(".submit")
-  button.addEventListener("click", (e)=>{
-    e.preventDefault();
-    getName(); 
-  })
-}
+//   let userInput = document.querySelector(".artistName").value
+//   for(let i = 0; i < songs.length; i++){
+//     if(songs[i].artists === `${userInput}`)
+//     displaySearch(songs[i]);  
+//   }
+//   return "Sorry That Artist Info is not available"; 
+// }; 
+
+
+
+// function search(){
+//   let button = document.querySelector(".submit")
+//   button.addEventListener("click", (e)=>{
+//     e.preventDefault();
+//     getName(); 
+//   })
+// }
 
 
 
@@ -276,7 +277,7 @@ function search(){
 // speechiness; tempo; 
 
 let variables = Object.keys(songs[0])
-const filteredVariables =  variables.filter(key => key !==  'src' && key !== 'release_date' && key !=='duration_ms' && key !=='id' && key !=='mode')
+const filteredVariables =  variables.filter(key => key !==  'src' && key !== 'release_date' && key !=='duration_ms' && key !=='id' && key !=='mode' && key !=='artists' && key !=='name')
 
 
 function options(){
@@ -297,7 +298,7 @@ function options(){
 function createCustomRows(arr, variable) {
   const tempRows = []; 
   for(let i = 0; i < arr.length; i++){
-    tempRows.push([arr[i].name + ", "+ arr[i].artist, arr[i][`${variable}`]])
+    tempRows.push([arr[i].name + ", "+ arr[i].artists, arr[i][`${variable}`]])
   }
   return tempRows
 };
@@ -307,17 +308,30 @@ function createCustomRows(arr, variable) {
 
 
 const customChart = (variable) => {
-  // anychart.onDocumentReady(function(variable) {
-    // const tempRows = []; 
-    // for(let i = 0; i < songs.length; i++){
-    //   tempRows.push([songs[i].name + ", "+ songs[i].artist, songs[i].variable])
-    // }
+  
+  let liChart = document.createElement('li');
+  liChart.setAttribute('class', 'custom'); 
+  liChart.setAttribute('key',`${count += 1}`)
 
+  let button = document.createElement('button')
+  button.setAttribute('class', 'button');
+  button.setAttribute('key',`${count}`)
+  button.innerHTML= 'Clear Graph'; 
 
+  button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    e.path[1].remove()
+  })
 
+  liChart.appendChild(button)
+  let ulList = document.querySelector('ul.graphs'); 
+  let container = ulList.appendChild(liChart)
+
+ 
+   
   anychart.theme(anychart.themes.darkBlue)
   // set the data
-  var data = {
+  let data = {
       header: ["Song Title", `${variable}`],
       rows: 
         createCustomRows(songs, variable)
@@ -334,9 +348,15 @@ const customChart = (variable) => {
   chart.title(`${variable} Levels, by Song`);
 
   // draw
-  chart.container(`custom`);
-  chart.draw();
+  // chart.container(`custom`);
+  chart.container(
+    container
+  );
+  chart.draw()
 
 }
 
 
+// deleteChart = () => {
+
+// }
